1483bd554b8a5c4edac1c42bbee24cf16f031178
gwanhyeon <kgh940525@gmail.com>
2021-09-16 20:58:13 +0900

    feat: JDBC Template and Data JPA

--- src/main/java/kgh/tacocloud/Ingredient.java
+++ src/main/java/kgh/tacocloud/Ingredient.java
@@ -1,14 +1,25 @@
 package kgh.tacocloud;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@Getter
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+
+@Data
 @RequiredArgsConstructor
+@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
+@Entity
 public class Ingredient {
+
+    @Id
     private final String id;
     private final String name;
     private final Type type;
 
+    public static enum Type {
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
 }
--- src/main/java/kgh/tacocloud/Taco.java
+++ src/main/java/kgh/tacocloud/Taco.java
@@ -3,18 +3,32 @@ package kgh.tacocloud;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Date;
 import java.util.List;
 
 @Setter
 @Getter
+@Entity
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Date createdAt;
     @NotNull
     @Size(min = 5, message = "5글자이상 입력하세요")
     private String name;
-    @Size(min = 1, message = "1개이상 재료를 선택하셔야합니다.")
-    private List<String> ingredients;
 
+    @ManyToMany(targetEntity = Ingredient.class)
+    @Size(min = 1, message = "1개이상 재료를 선택하셔야합니다.")
+    private List<Ingredient> ingredients;
+
+    @PrePersist
+    void createAt(){
+        this.createdAt = new Date();
+    }
 }
